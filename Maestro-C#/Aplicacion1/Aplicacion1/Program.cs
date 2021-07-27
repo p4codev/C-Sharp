0d057c43b29dc64c5de8ce0ef68edcce9c165da7
p4codev <franciscoalexander83@gmail.com>
2021-07-27 05:52:11 -0500

    Empezando Aprender C#

---
+++ Maestro-C#/Aplicacion1/Aplicacion1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplicacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
